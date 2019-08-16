de87753e60679d092a2dfbc2e8d31d6fd38a0657
Jonathan Ho <johnnyho82@gmail.com>
2019-08-16 13:36:58 -0400

    Build pagination system

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -11,18 +11,20 @@ const StyledHeader = styled.h1`
   top: 0;
   z-index: 9999;
   background-color: black;
-  color: #ffe919;
   margin: 0;
-  padding: 20px 0;
+  padding: 20px 0 10px 0;
   text-align: center;
   text-transform: uppercase;
   font-size: 60px;
+  text-shadow: -2px -2px 0 #ffe919, 2px -2px 0 #ffe919, -2px 2px 0 #ffe919,
+    2px 2px 0 #ffe919;
 `;
 
 const CardWrapper = styled.div`
   width: 90%;
   margin: 0 auto;
-  margin-top: 160px;
+  margin-top: 170px;
+  margin-bottom: 50px;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
@@ -32,25 +34,35 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [people, setPeople] = useState([]);
+  const [page, setPage] = useState(1);
+  const [pageCount, setPageCount] = useState();
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   useEffect(() => {
     axios
-      .get('https://swapi.co/api/people/')
+      .get(`https://swapi.co/api/people/?page=${page}`)
       .then(response => {
         setPeople(response.data.results);
+        // console.log(response.data);
+        setPageCount(response.data.count);
       })
       .catch(err => console.log(err));
-  }, []);
+  }, [page]);
 
-  console.log(people);
+  const prevPage = () => setPage(page - 1);
+  const nextPage = () => setPage(page + 1);
 
   return (
     <div className="App">
       <StyledHeader className="Header">React Wars</StyledHeader>
-      <Nav />
+      <Nav
+        page={page}
+        pageCount={pageCount}
+        prevPage={prevPage}
+        nextPage={nextPage}
+      />
       <CardWrapper>
         {people.map(character => {
           return <CharacterCard character={character} />;
--- starwars/src/components/Nav.js
+++ starwars/src/components/Nav.js
@@ -3,24 +3,50 @@ import styled from 'styled-components';
 
 const NavBar = styled.div`
   width: 100%;
-  top: 109px;
+  top: 99px;
   position: fixed;
   z-index: 100;
   background-color: black;
   color: #ffe919;
-  display: flex;
-  justify-content: space-around;
+`;
+
+const Center = styled.div`
+  width: 30%;
   margin: 0 auto;
-  margin-bottom: 20px;
-  padding-bottom: 20px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const PageTxt = styled.div`
+  font-size: 18px;
+`;
+
+const StyledButton = styled.button`
+  border: none;
+  background-color: black;
+  color: #ffe919;
+  font-size: 40px;
+  cursor: pointer;
+  outline: none;
 `;
 
 export default function Nav(props) {
   return (
     <NavBar>
-      <div>Prev</div>
-      <div>Page: </div>
-      <div>Next</div>
+      <Center>
+        {props.page > 1 && (
+          <StyledButton onClick={() => props.prevPage()}>
+            {'\u21E6'}
+          </StyledButton>
+        )}
+        <PageTxt>Page: {props.page}</PageTxt>
+        {props.page < props.pageCount && (
+          <StyledButton onClick={() => props.nextPage()}>
+            {'\u21E8'}
+          </StyledButton>
+        )}
+      </Center>
     </NavBar>
   );
 }
