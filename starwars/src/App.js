1f76e89be3d0905484bd198aa660d2900af7952d
Jonathan Ho <johnnyho82@gmail.com>
2019-08-16 11:44:26 -0400

    Fetch Star Wars characters and render components

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import CharacterCard from './components/CharacterCard';
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [people, setPeople] = useState([]);
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios
+      .get('https://swapi.co/api/people/')
+      .then(response => {
+        setPeople(response.data.results);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  console.log(people);
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      {/* <Nav /> */}
+      <div>
+        {people.map(character => {
+          return <CharacterCard character={character} />;
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ starwars/src/components/CharacterCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function CharacterCard(props) {
+  return (
+    <div>
+      <h1>{props.character.name}</h1>
+      <ul>
+        <li>Gender: {props.character.gender}</li>
+        <li>Height: {props.character.height}</li>
+        <li>Mass: {props.character.mass}</li>
+        <li>Hair: {props.character.hair_color}</li>
+        <li>Complexion: {props.character.skin_color}</li>
+        <li>Eyes: {props.character.eye_color}</li>
+        <li>Birth Year: {props.character.birth_year}</li>
+      </ul>
+    </div>
+  );
+}
